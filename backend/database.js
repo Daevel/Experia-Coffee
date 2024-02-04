8dfc84eecc77b0b83c6006362550e4c744af0a2c
Luigi Avitabile <luigi.avitabile5@gmail.com>
2024-02-04 16:08:16 +0100

    redefined docker-compose.yml

--- backend/database.js
+++ backend/database.js
@@ -5,9 +5,12 @@ var con = mysql.createConnection({
     host: process.env.DB_HOSTNAME,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABSE,
 });
 
 con.connect(function(err) {
     if (err) throw err;
     console.log("Database connected successfully!");
 })
+
+module.exports = con;
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const mysql = require('mysql');
 const app = express();
 const port = require("dotenv");
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'experia_coffee',
+});
+
+connection.connect();
 
 app.get("/", function (req, res) {
     res.send('{ "response": "Hello from Tech web daevel"}');
@@ -15,5 +24,8 @@ app.get("/ready", function (req, res) {
     res.send('{"response": "Great!, It works!"}');
 })
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
+
 module.exports = app;
